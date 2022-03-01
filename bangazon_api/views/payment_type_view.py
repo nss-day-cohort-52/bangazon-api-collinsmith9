9bc25b8a4bcad45f2e2fa69b59e2f282a790d93e
collin <geniuscollin@gmail.com>
2022-03-01 12:57:30 -0600

    fixed payment type bug, filtered payment types that are passed to serializer

--- bangazon_api/views/payment_type_view.py
+++ bangazon_api/views/payment_type_view.py
@@ -19,7 +19,8 @@ class PaymentTypeView(ViewSet):
     def list(self, request):
         """Get a list of payment types for the current user"""
         payment_types = PaymentType.objects.all()
-        serializer = PaymentTypeSerializer(payment_types, many=True)
+        filtered_payment_types = payment_types.filter(customer=request.user)
+        serializer = PaymentTypeSerializer(filtered_payment_types, many=True)
         return Response(serializer.data)
 
     @swagger_auto_schema(
